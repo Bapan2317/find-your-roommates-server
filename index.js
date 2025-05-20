71db320948465fdba0f1c457841f3333f41fe14e
Bapan2317 <bapanm2317@gmail.com>
2025-05-21 01:23:44 +0530

    Featured Roommates Post Section created

--- index.js
+++ index.js
@@ -24,12 +24,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const roommateCollection = client.db("find_roommate").collection("roommates")
+
+    // const showLimitRoommate = db.collection.find("find_roommate").limit(6)
+
+    app.get("/roommates", async(req, res) => {
+        const result = await roommateCollection.find({Availability: "available"}).limit(6).toArray()
+        res.send(result)
+    })
+
+    app.post("/roommates", async(req, res) => {
+    const newRoommate = req.body;
+    console.log(newRoommate);
+    const result = await roommateCollection.insertOne(newRoommate)
+    res.send(result)
+})
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -38,6 +56,8 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
+
+
 
 app.listen( port, () => {
     console.log(`Find roommate server is running on port ${port}`);
