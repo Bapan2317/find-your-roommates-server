19b5bace52bd699708bc9c2110f8bcdbdee049be
Bapan2317 <bapanm2317@gmail.com>
2025-05-22 19:47:45 +0530

    delete method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -34,12 +34,33 @@ async function run() {
         res.send(result)
     })
 
+    app.get("/allRoommates", async(req, res) => {
+        const result = await roommateCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get("/roommates/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await roommateCollection.findOne(query)
+        res.send(result)
+    })
+
     app.post("/roommates", async(req, res) => {
-    const newRoommate = req.body;
-    console.log(newRoommate);
-    const result = await roommateCollection.insertOne(newRoommate)
-    res.send(result)
-})
+      const newRoommate = req.body;
+      console.log(newRoommate);
+      const result = await roommateCollection.insertOne(newRoommate)
+      res.send(result)
+    })
+
+    app.delete("/roommates/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await roommateCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
 
     
     // Send a ping to confirm a successful connection
